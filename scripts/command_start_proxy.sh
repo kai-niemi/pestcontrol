ba77e181928285cc77f6bb96aff2da40a214348e
kai-niemi <kai.niemi.se@gmail.com>
2025-05-18 19:11:24 +0200

    -

--- scripts/command_start_proxy.sh
+++ scripts/command_start_proxy.sh
@@ -12,7 +12,8 @@ fi
 
 pid=$(ps -ef | grep "toxiproxy-server" | grep "host" | awk '{print $2}')
 if [ ! -x ${pid} ]; then
-   fn_print_info "toxiproxy-server (pid: ${pid}) appears to be running - listing proxies"
+   fn_print_info "toxiproxy-server (pid: ${pid}) appears to be running already"
+   fn_fail_check toxiproxy-cli list
 else
   if fn_prompt_yes_no "Start toxiproxy-server?" Y; then
     fn_fail_check nohup toxiproxy-server -host ${toxiproxy_host} -port ${toxiproxy_port} > toxiproxy-stdout.log 2>&1 &
@@ -21,25 +22,22 @@ else
   fi
 fi
 
-fn_fail_check toxiproxy-cli list
+if fn_prompt_yes_no "Start toxiproxy clients?" Y; then
+  fn_local_node_range
 
-fn_print_info "Add up to ${#locality_zone[@]} node proxies automatically"
-fn_local_node_range
+  for node in $nodes
+  do
+      let offset=${node}-1
+      let rpcport=${rpcportbase}+$offset
 
-for node in $nodes
-do
-    let offset=${node}-1
-    let rpcport=${rpcportbase}+$offset
+      if [ "${toxiproxy}" != "off" ]; then
+        let advertise_rpcport=${toxiproxy_advertiseportbase}+$offset
+      else
+        let advertise_rpcport=${rpcportbase}+$offset
+      fi
 
-    if [ "${toxiproxy}" != "off" ]; then
-      let advertise_rpcport=${toxiproxy_advertiseportbase}+$offset
-    else
-      let advertise_rpcport=${rpcportbase}+$offset
-    fi
+      fn_fail_check toxiproxy-cli create --listen ${toxiproxy_host}:${advertise_rpcport} --upstream ${host}:${rpcport} crdb-${node}
+  done
 
-    fn_fail_check toxiproxy-cli create --listen ${toxiproxy_host}:${advertise_rpcport} --upstream ${host}:${rpcport} crdb-${node}
-done
-
-fn_fail_check toxiproxy-cli list
-
-fn_print_ok "Started toxiproxy server with proxies - you can now start the CockroachDB cluster!"
+  fn_fail_check toxiproxy-cli list
+fi
